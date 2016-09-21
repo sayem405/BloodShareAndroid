a2230d4135d28a07c3d6971abac8ee3b0329d5ef
sayem405 <sayem.cse18@gmail.com>
2016-09-21 22:54:02 +0600

    initial commit
    initial commit for basic structure

---
+++ app/src/main/java/com/bloodshare/bloodshareandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bloodshare.bloodshareandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
