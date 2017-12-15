ca31d6f2fda7972cd989fda70810bb6911e91a36
M.A. Sayem <sayem.cse18@gmail.com>
2017-12-16 00:39:03 +0600

    bug fixed : update user

--- app/src/main/java/com/bloodshare/bloodshareandroid/data/model/DonorLocation.java
+++ app/src/main/java/com/bloodshare/bloodshareandroid/data/model/DonorLocation.java
@@ -6,7 +6,6 @@ import android.arch.persistence.room.Ignore;
 
 import com.bloodshare.bloodshareandroid.data.db.DBNamesFields;
 import com.google.android.gms.location.places.Place;
-import com.google.android.gms.maps.model.LatLng;
 
 public class DonorLocation {
 
@@ -16,15 +15,15 @@ public class DonorLocation {
     @ColumnInfo(name = DBNamesFields.COLUMN_LONGITUDE)
     public double longitude;
 
-    @ColumnInfo(name = DBNamesFields.COLUMN_LOCATION)
-    public String location;
+    @ColumnInfo(name = DBNamesFields.COLUMN_NAME)
+    public String name;
 
     public DonorLocation() {
     }
 
     @Ignore
     public DonorLocation(Place place) {
-        this.location = place.getName().toString();
+        this.name = place.getName().toString();
         this.latitude = place.getLatLng().latitude;
         this.longitude = place.getLatLng().longitude;
     }
---
+++ app/src/main/java/com/bloodshare/bloodshareandroid/data/network/SerializedNameFields.java
@@ -0,0 +1,9 @@
+package com.bloodshare.bloodshareandroid.data.network;
+
+/**
+ * Created by sayem on 12/15/2017.
+ */
+
+public class SerializedNameFields {
+    public static final String LOCATION = "location";
+}
